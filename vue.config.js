1cc018d962912c007ca7af73f866e1567ed4202c
Jimmywei01 <min1990719@gmail.com>
2019-05-03 17:44:08 +0800

    update

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,4 @@
 module.exports = {
-  publicPath: process.env.NODE_ENV === 'production' ?
-    '/Toys/dist/' :
-    '/',
+  publicPath: process.env.NODE_ENV === 'production' ? '/Toys/dist/' : './',
+  outputDir: 'dist'
 }
