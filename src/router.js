c9feb0ce43de87514eae89c2bdd87fa819b18198
Jimmywei01 <min1990719@gmail.com>
2019-05-03 16:24:30 +0800

    update

--- src/router.js
+++ src/router.js
@@ -1,20 +1,5 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Home from './views/Home.vue'
-import Login from './views/Login.vue'
-import Dashboard from './views/Dashboard.vue'
-
-import Products from './components/dashboard/Products.vue'
-import Orders from './components/dashboard/Orders.vue'
-import Coupons from './components/dashboard/Coupons.vue'
-import CustomeOrders from './components/dashboard/CustomeOrders.vue'
-import CustomeCheckOut from './components/dashboard/CustomeCheckOut.vue'
-import ShoppingDetail from './components/HomePage/ShoppingDetail.vue'
-import Cart from './components/HomePage/Cart.vue'
-import CheckOrder from './components/HomePage/CheckOrder.vue'
-import CheckOut from './components/HomePage/CheckOut.vue'
-import Page from './components/HomePage/Page.vue'
-import ShoppingList from './components/HomePage/ShoppingList.vue'
 Vue.use(Router)
 
 export default new Router({
@@ -26,77 +11,77 @@ export default new Router({
     },
     {
       path: '/',
-      component: Home,
+      component: () => import('./views/Home.vue'),
       children: [
         {
           path: '/',
           name: 'Page',
-          component: Page
+          component: () => import('./components/HomePage/Page.vue')
         },
         {
           path: 'shoppinglist',
           name: 'ShoppingList',
-          component: ShoppingList
+          component: () => import('./components/HomePage/ShoppingList.vue')
         },
         {
           path: 'shoppingdetail/:id',
           name: 'ShoppingDetail',
-          component: ShoppingDetail
+          component: () => import('./components/HomePage/ShoppingDetail.vue')
         },
         {
           path: 'cart',
           name: 'Cart',
-          component: Cart
+          component: () => import('./components/HomePage/Cart.vue')
         },
         {
           path: 'checkorder',
           name: 'CheckOrder',
-          component: CheckOrder
+          component: () => import('./components/HomePage/CheckOrder.vue')
         },
         {
           path: 'checkout/:orderId',
           name: 'CheckOut',
-          component: CheckOut
+          component: () => import('./components/HomePage/CheckOut.vue')
         }]
     },
     {
       path: '/login',
       name: 'Login',
-      component: Login
+      component: () => import('./views/Login.vue')
     },
     // 後台
     {
       path: '/dashboard',
       name: 'Dashboard',
-      component: Dashboard,
+      component: () => import('./views/Dashboard.vue'),
       meta: { requiresAuth: true },
       children: [{
         path: 'products',
         name: 'Products',
-        component: Products,
+        component: () => import('./components/dashboard/Products.vue'),
         meta: { requiresAuth: true }
       },
       {
         path: 'orders',
         name: 'Orders',
-        component: Orders,
+        component: () => import('./components/dashboard/Orders.vue'),
         meta: { requiresAuth: true }
       },
       {
         path: 'coupons',
         name: 'Coupons',
-        component: Coupons,
+        component: () => import('./components/dashboard/Coupons.vue'),
         meta: { requiresAuth: true }
       },
       {
         path: 'customeorders',
         name: 'CustomeOrders/:id',
-        component: CustomeOrders
+        component: () => import('./components/dashboard/CustomeOrders.vue')
       },
       {
         path: 'customecheckout/:orderId',
         name: 'CustomeCheckOut',
-        component: CustomeCheckOut
+        component: () => import('./components/dashboard/CustomeCheckOut.vue')
       }]
     }]
 })
--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,5 @@
 module.exports = {
-  publicPath: '/Toys/dist/'
+  publicPath: process.env.NODE_ENV === 'production' ?
+    '/Toys/' :
+    '/',
 }
